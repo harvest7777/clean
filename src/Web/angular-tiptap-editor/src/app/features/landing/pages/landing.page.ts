6907859c24e75be1aa594666d834ab1b30bae6fd
harvest7777 <333harvest.moon333@gmail.com>
2026-05-04 14:41:49 -0700

    feat: add InputComponent to landing page and make button width flexible

--- src/Web/angular-tiptap-editor/src/app/features/landing/pages/landing.page.ts
+++ src/Web/angular-tiptap-editor/src/app/features/landing/pages/landing.page.ts
@@ -3,11 +3,12 @@ import { AppRouteUrls } from "../../../core/routing/app-routes";
 import { RouterLink } from "@angular/router";
 import { AuthButtonContainerComponent } from "../../../core/auth/components/auth-button-container/auth-button-container.component";
 import { TerminalSpinnerComponent } from "../../../ui/components/terminal-spinner/terminal-spinner.component";
+import { InputComponent } from "../../../ui/components/input/input.component";
 
 @Component({
   selector: "app-landing-page",
   standalone: true,
-  imports: [RouterLink, AuthButtonContainerComponent, TerminalSpinnerComponent],
+  imports: [RouterLink, AuthButtonContainerComponent, TerminalSpinnerComponent, InputComponent],
   templateUrl: "./landing.page.html",
 })
 export class LandingPageComponent {
